b9a4f38d0419fe6d95f13b9b881273f27e5ae75d
akira225 <43086184+akira225@users.noreply.github.com>
2022-05-30 02:00:16 +0300

    add connection check

--- backend/WikiRpcClient.py
+++ backend/WikiRpcClient.py
@@ -6,15 +6,6 @@ import json
 class WikiRpcClient:
 
     def __init__(self):
-        self.corr_id = None
-        self.response = None
-        self.callback_queue = None
-        self.channel = None
-        self.connection = None
-
-    def refresh_state(self):
-        if self.connection:
-            self.connection.close()
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host='127.0.0.1'))
         self.channel = self.connection.channel()
@@ -27,12 +18,36 @@ class WikiRpcClient:
             on_message_callback=self.on_response,
             auto_ack=True)
 
+    def basic_check(self):
+        if not self.connection or self.connection.is_closed:
+            self.connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host='127.0.0.1'))
+            self.channel = self.connection.channel()
+
+            result = self.channel.queue_declare(queue='')
+            self.callback_queue = result.method.queue
+
+            self.channel.basic_consume(
+                queue=self.callback_queue,
+                on_message_callback=self.on_response,
+                auto_ack=True)
+        if not self.channel or self.channel.is_closed:
+            self.channel = self.connection.channel()
+
+            result = self.channel.queue_declare(queue='')
+            self.callback_queue = result.method.queue
+
+            self.channel.basic_consume(
+                queue=self.callback_queue,
+                on_message_callback=self.on_response,
+                auto_ack=True)
+
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
             self.response = body
 
     def call_validate(self, article):
-        self.refresh_state()
+        self.basic_check()
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(
@@ -48,7 +63,7 @@ class WikiRpcClient:
         return json.loads(self.response)
 
     def call_path(self, A, B):
-        self.refresh_state()
+        self.basic_check()
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(
